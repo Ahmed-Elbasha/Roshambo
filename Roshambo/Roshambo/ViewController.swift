703464cf6ae824b832d25eb29f2c1f6faffb5960
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-06-26 12:18:31 +0200

    Added UI Design to HomeViewController & Modified Assets

--- Roshambo/Roshambo/ViewController.swift
+++ Roshambo/Roshambo/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var paperButton: UIButton!
+    @IBOutlet weak var scissorsButton: UIButton!
+    @IBOutlet weak var rockButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func rockButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func paperButtonPressed(_ sender: Any) {
+    }
+    
 }
 

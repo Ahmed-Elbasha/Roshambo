ea2f926a3d7eb59c0b2742392e55ba9520c5cfb8
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-06-27 17:29:32 +0200

    Added Logic to NewGameViewController

---
+++ Roshambo/Roshambo/GameResultViewController.swift
@@ -0,0 +1,38 @@
+//
+//  GameResultViewController.swift
+//  Roshambo
+//
+//  Created by Ahmed Elbasha on 6/27/18.
+//  Copyright © 2018 Ahmed Elbasha. All rights reserved.
+//
+
+import UIKit
+
+class GameResultViewController: UIViewController {
+    
+    var playerChoice : Int = 0
+    var randomValue : Int = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Roshambo/Roshambo/NewGameViewController.swift
+++ Roshambo/Roshambo/NewGameViewController.swift
@@ -14,6 +14,14 @@ class NewGameViewController: UIViewController {
     @IBOutlet weak var scissorsButton: UIButton!
     @IBOutlet weak var rockButton: UIButton!
     
+    enum PlayerChoice : Int {
+        case choicePaper = 1,
+         choiceRock = 2,
+         choiceScissors = 3
+    }
+    
+    var playerChoice : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -25,10 +33,36 @@ class NewGameViewController: UIViewController {
     }
 
     @IBAction func rockButtonPressed(_ sender: Any) {
+        playerChoice = PlayerChoice.choiceRock.rawValue
+        
+        let gameResultVC = storyboard?.instantiateViewController(withIdentifier: "GameResultViewController") as! GameResultViewController
+        
+        gameResultVC.playerChoice = self.playerChoice
+        gameResultVC.randomValue = generateRandomValue()
+        
+        self.present(gameResultVC, animated: true, completion: nil)
+    }
+    
+    func generateRandomValue() -> Int {
+        let randomValue = 1 + arc4random() / 3
+        return Int(randomValue)
     }
     
     @IBAction func paperButtonPressed(_ sender: Any) {
+        playerChoice = PlayerChoice.choicePaper.rawValue
+        self.performSegue(withIdentifier: "paperClicked", sender: self)
     }
     
-}
+    @IBAction func scissorsbuttonWasPresssed(_ sender: Any) {
+        playerChoice = PlayerChoice.choiceScissors.rawValue
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "scissorsClicked" || segue.identifier == "paperClicked") {
+            let gameResultVC = segue.destination as! GameResultViewController
+            gameResultVC.playerChoice = self.playerChoice
+            gameResultVC.randomValue = generateRandomValue()
+        }
+    }
 
+}
